493738bf63e787b01092be05123bcef9d4c39b98
thecodinglife <ttll442003@gmail.com>
2024-09-15 15:52:28 +0700

    finish

--- F2CC2F/app/src/main/java/com/hfad/f2cc2f/MainViewModel.java
+++ F2CC2F/app/src/main/java/com/hfad/f2cc2f/MainViewModel.java
@@ -5,14 +5,22 @@ import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
 
+    public String doC = "";
+    public String doF = "";
+    public MutableLiveData<String> C2F= new MutableLiveData<>("");
+    public MutableLiveData<String>  F2C = new MutableLiveData<>("");
 
     public void f2c(){
+
+        String formatStr = String.format("%.1f", (Float.parseFloat(doF) - 32) * ((float) 5 / 9));
+        F2C.setValue(formatStr);
+
     }
 
     public void c2f(){
-    }
 
-    public void clear(){
+        String formatStr = String.format("%.1f", Float.parseFloat(doC) * 1.8f + 32);
+        C2F.setValue(formatStr);
 
     }
 }
