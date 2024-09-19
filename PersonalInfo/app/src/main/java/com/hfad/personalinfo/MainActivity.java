de341a32555861dce1aa9e182c1a111fc31db4a7
thecodinglife <ttll442003@gmail.com>
2024-09-19 14:59:47 +0700

    hoan thanh app

--- PersonalInfo/app/src/main/java/com/hfad/personalinfo/MainActivity.java
+++ PersonalInfo/app/src/main/java/com/hfad/personalinfo/MainActivity.java
@@ -2,7 +2,9 @@ package com.hfad.personalinfo;
 
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -19,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
         //set databinding variable
         MainViewModel viewModel = new MainViewModel();
         binding.setViewmodel(viewModel);
+        binding.setLifecycleOwner(this);
+
+        //set event
+        binding.submit.setOnClickListener(v->{
+            String info = viewModel.submit();
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setTitle("Thông tin cá nhân");
+            builder.setPositiveButton("Đóng", (dialog, which) -> dialog.cancel());
+            builder.setMessage(info);
+            builder.create().show();
+        });
+
+        //custom back navigation
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+//                Log.d("back", "back");
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Thông báo");
+                builder.setIcon(R.drawable.baseline_warning_24);
+                builder.setMessage("Bạn có muốn thoát ứng dụng?");
+                builder.setPositiveButton("Co", (dialog, which)-> finish());
+                builder.setNegativeButton("Khong", (dialog, which)->dialog.cancel());
+                builder.create().show();
+                }
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
     }
 }
--- PersonalInfo/app/src/main/java/com/hfad/personalinfo/MainViewModel.java
+++ PersonalInfo/app/src/main/java/com/hfad/personalinfo/MainViewModel.java
@@ -3,4 +3,38 @@ package com.hfad.personalinfo;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
+
+    public boolean trungCap = false;
+    public boolean daiHoc = false;
+    public boolean caoDang = false;
+    public boolean docSach = false;
+    public boolean ngheNhac = false;
+    public boolean xemPhim = false;
+    public String extra = "";
+    public String ten="";
+    public String cmnd="";
+
+    public String submit(){
+
+        String info;
+        String bangCap="";
+        StringBuilder soThich = new StringBuilder();
+
+        if(trungCap) bangCap = "Trung cap";
+        else if (daiHoc) bangCap = "Dai hoc";
+        else if (caoDang) bangCap = "Cao dang";
+
+        if(docSach) soThich.append("Doc sach");
+        if (ngheNhac) soThich.append("Nghe nhac");
+        if(xemPhim) soThich.append("Xem phim");
+
+        info = ten+'\n'
+                +cmnd+'\n'
+                +bangCap+'\n'
+                +soThich+'\n'
+                +extra+'\n';
+
+        return info;
+    }
+
 }
