75b64324be9608a5d7468fdabab15b5a0d09ffe7
thecodinglife <ttll442003@gmail.com>
2024-09-28 14:45:34 +0700

    hoan thanh app

--- prac01/app/src/main/java/com/thuchanh/prac01/MainActivity.java
+++ prac01/app/src/main/java/com/thuchanh/prac01/MainActivity.java
@@ -1,5 +1,6 @@
 package com.thuchanh.prac01;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -8,15 +9,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.thuchanh.prac01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 111;
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //set layout
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //set text
-        binding.textView.setText("Hello World");
+        //
+        binding.button2.setOnClickListener(v -> {
+            Intent intent = new Intent(this, SubActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                String name = data.getStringExtra("name");
+                String gpa = data.getStringExtra("gpa");
+                binding.txt.setText("Ho va ten: " + name +
+                        '\n'+"GPA: " + gpa);
+            }
+        }
     }
 }
--- prac01/app/src/main/java/com/thuchanh/prac01/SubActivity.java
+++ prac01/app/src/main/java/com/thuchanh/prac01/SubActivity.java
@@ -1,6 +1,32 @@
 package com.thuchanh.prac01;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.thuchanh.prac01.databinding.ActivitySubBinding;
+
 public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //set layout
+        ActivitySubBinding binding = ActivitySubBinding.inflate(getLayoutInflater());
+
+        binding.button.setOnClickListener(v -> {
+            Intent resultIntent = new Intent();
+
+            // put your result data
+            resultIntent.putExtra("name", binding.name.getText().toString());
+            resultIntent.putExtra("gpa", binding.gpa.getText().toString());
+            setResult(Activity.RESULT_OK, resultIntent);
+            finish();
+        });
+        setContentView(binding.getRoot());
+    }
 }
